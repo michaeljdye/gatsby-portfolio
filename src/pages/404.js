ed17cf12b48816a2451c3c2792ea1325c892dc14
Joey Dye <joey.m.dye@gmail.com>
2019-05-14 20:03:20 -0500

    fix: browser compatability issues

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,7 @@ import Layout from '../components/Layout'
 const notFoundPage = () => (
   <Layout>
     <Container>
-      <h1>NOT FOUND</h1>
+      <h1>Not Found</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     </Container>
   </Layout>
